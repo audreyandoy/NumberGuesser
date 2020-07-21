412c8c7c025367fe938bcfbecf8a386aad5c0e8e
Audrey Andoy <audreylandoy@gmail.com>
2020-07-21 18:41:46 +0000

    game logic to see if player num is bigger or less than computer num.

--- main.py
+++ main.py
@@ -8,10 +8,32 @@ print("Welcome to Number Guesser")
 # - Tries
 # - TriesOver
 # - Check player won
-
+# - TypeError Handling
 
 #global variables
 comp_num = random.randint(0, 10)
 tries = 3
 won = False
 play = True
+
+while play == True:
+  while tries > 0:
+    print()
+    player_num = input("Enter a number between 0-10: ")
+    player_num = float(player_num)
+    if player_num < 0 or player_num > 10:
+      print("Bad Number")
+      break
+    else:
+      if player_num > comp_num:
+        print("Number too big")
+        tries -= 1
+        print("You have "+ str(tries) + " tries left")
+      elif player_num < comp_num:
+        print("Number too small")
+        tries -= 1
+        print("You have "+ str(tries) + " tries left")
+      else:
+        print("Correct")
+        won = True
+        break
