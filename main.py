07dc48fb3297075243f312322e235ef0d96913f1
Audrey Andoy <audreylandoy@gmail.com>
2020-07-21 17:34:45 +0000

    game setup with variables

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import random
 
 print("Welcome to Number Guesser")
+
+#PROJECT PLAN W CLASS
+# - Computer Number
+# - Player number
+# - Tries
+# - TriesOver
+# - Check player won
+
+
+#global variables
+comp_num = random.randint(0, 10)
+tries = 3
+won = False
+play = True
